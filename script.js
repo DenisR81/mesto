0fd30ac5503ce994fbc990386021e5d254cbb0e1
Denis Reshetnyak <denresh81@yandex.ru>
2022-01-21 02:08:19 +0700

    mesto 2

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-const openPopupButton = document.querySelector('.edit-button');
+const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.btn-close');
-const savePopupButton = document.querySelector('.popup__btn-save');
 const popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.getElementById('edit-name');
+let jobInput = document.getElementById('edit-work');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
 
-function openPopup(event) {
-  event.preventDefault();
+
+
+function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__work');
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let userName = nameInput.value;
-  let userJob = jobInput.value;
-  let profileName = document.querySelector('.profile__title');
-  let profileJob = document.querySelector('.profile__subtitle');
-  profileName.textContent = userName;
-  profileJob.textContent = userJob;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
   closePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
